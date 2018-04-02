71c2447bb89189b3f76df9ab3d98afb0fee2d8b6
Fotis Pegios <fotispeg@gmail.com>
2018-04-02 12:19:54 +0200

    insert functions to new function.js file

---
+++ src/scripts/functions.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    includeComponents();
+});
+
+$(window).on('load', function() {
+
+});
+
+// generate html components
+function includeComponents() { 
+    var components = $('[data-component]');
+    $.each(components, function(){
+        var file = 'dist/html/' + $(this).data('component') + '.html';
+        $(this).load(file, function() {
+            initClickEvents();
+        });
+    });
+}
+
+// hide the active component and show the selected
+function showComponent(component) {
+    $(".active").removeClass("active").addClass("hidden");
+    $("[data-component=" + component + "]").addClass("active").removeClass("hidden");
+}
